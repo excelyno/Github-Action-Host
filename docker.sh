1651ac8fc5b0c9b49210d4889eebf32734a476ce
excelyno <excellentqweee@gmail.com>
2024-10-31 23:45:52 +0700

    menambahkan fitur auto rm

--- docker.sh
+++ docker.sh
@@ -1,1 +1,6 @@
-pwd
+
+sudo su 
+docker stop github-action-host-wordpress-1 github-action-host-db-1
+docker rm github-action-host-wordpress-1 github-action-host-db-1
+cd ../
+rm -rf Github-Action-Host
